84b46826e06020057be263d4a1d2da1781f573e8
tjohn252 <tjohn252@uncc.edu>
2020-08-11 21:35:07 -0400

    made mobile version of navigation (still needs toggle functionality)

--- comp-files/menu.php
+++ comp-files/menu.php
@@ -1,5 +1,12 @@
 <nav>
   <ul>
+    <a href="#">
+      <svg id="h-bar" viewBox="0 0 100 100" width="5%" height="40">
+        <rect width="100" height="15" rx="8" fill="#0e4d44"></rect>
+        <rect y="30" width="100" height="15" rx="8" fill="#0e4d44"></rect>
+        <rect y="60" width="100" height="15" rx="8" fill="#0e4d44"></rect>
+      </svg>
+    </a>
     <li id="nav-home"><a href="#">Home</a></li>
     <li id="nav-about"><a href="#">About Us</a></li>
     <li id="nav-pastor"><a href="#">Our Pastor</a></li>
@@ -7,11 +14,4 @@
     <li id="nav-events"><a href="#">Announcements</a></li>
     <li id="nav-contact"><a href="#">Contact Us</a></li>
   </ul>
-  <a href="#">
-    <svg id="h-bar" viewBox="0 0 100 100" width="5%" height="40">
-      <rect width="100" height="15" rx="8" fill="#0e4d44"></rect>
-      <rect y="30" width="100" height="15" rx="8" fill="#0e4d44"></rect>
-      <rect y="60" width="100" height="15" rx="8" fill="#0e4d44"></rect>
-    </svg>
-  </a>
 </nav>
