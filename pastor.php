7790cf9d38e18d2ec79bc4a9f3575de55e579907
tjohn252 <tjohn252@uncc.edu>
2020-08-13 21:53:38 -0400

    added pseudo content to about and pastor pages and created stylings

--- pastor.php
+++ pastor.php
@@ -21,8 +21,28 @@
 
 
         <!-- Content Begins Here -->
+        <h1 style="padding-top: 45px;"><span id="pastor-sub" style="font-size: 20px; font-weight: normal;"><br>Our Pastor</span><br> Sup. Samuel L Payne, Jr.</h1>
+        <div class="about-text-wrapper">
 
 
+          <h2>Who is Sup. Payne?</h2>
+          <p>&emsp; &emsp; Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vel consectetur dolor. Fusce tempus, quam sit amet rutrum venenatis, ex nibh auctor est, in hendrerit metus lectus at arcu. Suspendisse sagittis nisi in ex consequat lacinia. Duis malesuada velit ut est suscipit fermentum. Donec odio lorem, mattis sed eros quis, volutpat condimentum erat. Mauris et ullamcorper justo. Aliquam vel cursus purus, a congue elit. Nunc dignissim, mauris in tempus faucibus, dui risus sodales ligula, sit amet feugiat tellus ipsum non nunc. Nullam eu eleifend lorem. Nunc pellentesque sodales ipsum, nec hendrerit velit condimentum non. Mauris sagittis turpis libero, a finibus mi lobortis id. Aenean tincidunt massa a eleifend feugiat. Donec condimentum dolor vel eros bibendum aliquam. Ut consectetur, mi laoreet consequat iaculis, tellus dui sodales magna, sit amet egestas nunc lorem ac tortor. Fusce libero odio, dictum nec neque quis, aliquam porta felis. Curabitur in vulputate metus. <br>&emsp; &emsp;
+
+        Suspendisse ante dolor, volutpat et enim lacinia, mattis eleifend orci. Pellentesque luctus fringilla rhoncus. Vivamus eget arcu id risus ullamcorper interdum a sit amet massa. Etiam laoreet ut lectus ut tristique. Maecenas nec fermentum tellus. Ut lorem orci, interdum sed justo quis, suscipit convallis tellus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aliquam lorem sapien, scelerisque ac turpis pellentesque, condimentum commodo leo.</p>
+
+
+          <h2>Bio</h2>
+
+          <p>&emsp; &emsp; Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sed fringilla tellus, ut varius ante. Praesent condimentum metus ac eros pretium, sit amet tristique nibh luctus. Vestibulum bibendum, quam quis vestibulum pulvinar, lectus neque sollicitudin lectus, eget tincidunt sem risus ac leo. Donec sed orci vulputate, pulvinar tortor ac, sollicitudin velit. Mauris eget sagittis justo, id porta sem. Quisque porta congue nibh id pulvinar. Nunc gravida diam et leo auctor elementum.<br>&emsp; &emsp;
+
+        Mauris cursus sapien sit amet lorem condimentum, a congue erat viverra. Etiam vel dolor quis lacus auctor ultricies. Morbi at condimentum augue. Quisque lacus mauris, volutpat at porta eu, suscipit vel velit. Ut vitae ex pellentesque, commodo felis vitae, varius sapien. Ut vel blandit sem. Quisque nec nisi turpis.<br>&emsp; &emsp;
+
+        Nunc quam orci, volutpat et magna vel, dapibus tincidunt mi. Aliquam hendrerit augue venenatis nulla rutrum, vitae porttitor magna hendrerit. Phasellus ac tempor tortor. Interdum et malesuada fames ac ante ipsum primis in faucibus. In hac habitasse platea dictumst. Curabitur sed arcu quis odio ultrices pellentesque blandit ac elit. Aliquam sodales justo id sapien ultricies rutrum. Ut eget dui quam. Praesent eget est vitae nulla condimentum finibus. Fusce risus nisi, pulvinar sed mauris vel, sollicitudin auctor turpis. In hac habitasse platea dictumst. Ut egestas, enim at faucibus tincidunt, nulla dui ornare odio, sit amet feugiat nibh augue quis elit.</p>
+
+          <h2>First Lady Janice Marie Payne</h2>
+
+          <p>&emsp; &emsp; Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque lacinia turpis ut arcu fermentum, eget lacinia ligula finibus. Fusce posuere est sodales erat sagittis, sit amet posuere quam hendrerit. Cras convallis ex quis neque dictum volutpat in ut libero. In rhoncus lectus sed velit sagittis, id tristique neque ultricies. Nunc non nunc in lectus laoreet lobortis. Nullam egestas velit cursus, euismod massa quis, suscipit nisi. Proin ultrices bibendum neque, non auctor odio consequat eget. Phasellus consectetur nunc sit amet sapien lobortis aliquet. Curabitur vel tellus id mi dictum volutpat at eu nunc. Vivamus dui odio, pellentesque et justo sed, semper dapibus felis. Sed cursus erat nec nulla commodo faucibus. Ut in elit non arcu sodales maximus sit amet non leo. Proin ac interdum sem.</p>
+        </div>
 
 
         <!-- Content Ends Here -->
