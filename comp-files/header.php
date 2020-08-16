68573e1382000d0bd08300d3f9fb1984ad811408
tjohn252 <tjohn252@uncc.edu>
2020-08-15 20:38:43 -0400

    sucessfully split main css into css section files

--- comp-files/header.php
+++ comp-files/header.php
@@ -1,10 +1,17 @@
     <meta charset="utf-8">
     <meta name=”viewport” content=”width=device-width, initial-scale=1.0”>
+
+    <!-- Reset and general stylings-->
     <?php include ('styles/reset.css') ?>
     <?php include ('styles/style.css') ?>
-    <?php include ('styles/footer.css') ?>
     <?php include ('styles/container.css') ?>
+
+    <!-- Page element stylings -->
+    <?php include ('styles/nav.css') ?>
     <?php include ('styles/quick-links.css') ?>
     <?php include ('styles/rotating-banner.css') ?>
     <?php include ('styles/about.css') ?>
-    <?php include ('styles/nav.css') ?>
+    <?php include ('styles/front-banner.css') ?>
+
+    <!-- Footer stylings -->
+    <?php include ('styles/footer.css') ?>
