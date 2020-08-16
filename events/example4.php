8b51791e400cddb5c0d4d18da2458b0eba1c69e1
tjohn252 <tjohn252@uncc.edu>
2020-08-16 19:46:53 -0400

    tested multiple events and adjusted stylings accordingly

---
+++ events/example4.php
@@ -0,0 +1,18 @@
+<div class="event-wrapper">
+
+  <img src="img/events/example4.jpg" alt="">
+
+  <div class="event-text-wrapper">
+    <h5 class="event-title"> Event Title 4</h5>
+    <p class="event-text"> <span class="event-data">Date: </span> 2020-08-21</p>
+    <p class="event-text"> <span class="event-data">Time: </span> 19:08 </p>
+    <p class="event-text"> <span class="event-data">Location: </span> Zoom Meeting: <a href="#"> here </a></p>
+    <p class="event-text"> <span class="event-data">Details: </span>
+
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et libero vitae ipsum suscipit tempor. Nunc facilisis orci eget feugiat hendrerit. Integer fermentum dolor eros, ut sollicitudin ipsum pellentesque sit amet. Ut orci arcu, efficitur vitae pharetra sit amet, auctor quis nisl. Ut non sapien eros. Mauris cursus, arcu vel vestibulum elementum, tortor turpis tristique massa, at feugiat mauris sapien ac dolor. Integer pretium tempus turpis, nec ultricies nibh bibendum a.
+
+  </p>
+
+  </div>
+
+</div>
