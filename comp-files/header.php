6addb376a470e0b735d3287d2c30b34999599a43
tjohn252 <tjohn252@uncc.edu>
2020-08-15 20:28:43 -0400

    created different stylesheets for different sections

--- comp-files/header.php
+++ comp-files/header.php
@@ -2,3 +2,9 @@
     <meta name=”viewport” content=”width=device-width, initial-scale=1.0”>
     <?php include ('styles/reset.css') ?>
     <?php include ('styles/style.css') ?>
+    <?php include ('styles/footer.css') ?>
+    <?php include ('styles/container.css') ?>
+    <?php include ('styles/quick-links.css') ?>
+    <?php include ('styles/rotating-banner.css') ?>
+    <?php include ('styles/about.css') ?>
+    <?php include ('styles/nav.css') ?>
