dbe9670081f3749e751ce8600f0f2da250ef2b34
tjohn252 <tjohn252@uncc.edu>
2020-11-29 22:23:52 -0500

    minor alignment changes

--- contact-form-send.php
+++ contact-form-send.php
@@ -1,21 +1,22 @@
 <?php
 
+  if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $user_email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+    $message = wordwrap($message, 75);
 
-  $name = $_POST['name'];
-  $user_email = $_POST['email'];
-  $subject = $_POST['subject'];
+    $body = "Name:".$name."\n
+             Email:".$user_email."\n
+             Message:".$message;
 
-  $message = $_POST['message'];
-  $message = wordwrap($message, 75);
-  $body = "Name:".$name."\n
-           Email:".$user_email."\n
-           Message:".$message;
-
-  $reciever = 'admin@sycamoretemplecogic.org';
-
-
-  mail($reciever, $subject, $body);
-  header("Location: index.php");
+    $reciever = 'admin@sycamoretemplecogic.org';
 
 
+    mail($reciever, $subject, $body, "From".$user_email);
+    header("Location: index.php");
+  } else {
+    echo "<h1>Did not work...<h1>";
+  }
 ?>
