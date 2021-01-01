854a11b49f4be7958da92f63ac8af4c5a68f7a1b
tjohn252 <tjohn252@uncc.edu>
2021-01-01 13:18:20 -0500

    fixed navigation bug that scrolled to top

--- comp-files/menu.php
+++ comp-files/menu.php
@@ -1,12 +1,10 @@
 <nav>
   <ul>
-    <a href="#" onclick="navToggle()">
-      <svg id="h-bar" viewBox="0 0 100 100" width="5%" height="64">
-        <rect width="100" height="15" rx="8" fill="#0e4d44"></rect>
-        <rect y="30" width="100" height="15" rx="8" fill="#0e4d44"></rect>
-        <rect y="60" width="100" height="15" rx="8" fill="#0e4d44"></rect>
-      </svg>
-    </a>
+    <svg id="h-bar" viewBox="0 0 100 100" width="5%" height="64" onclick="navToggle()">
+      <rect width="100" height="15" rx="8" fill="#0e4d44"></rect>
+      <rect y="30" width="100" height="15" rx="8" fill="#0e4d44"></rect>
+      <rect y="60" width="100" height="15" rx="8" fill="#0e4d44"></rect>
+    </svg>
     <li id="nav-home"><a href="index.php">Home</a></li>
     <li id="nav-about"><a href="about.php">About Us</a></li>
     <li id="nav-pastor"><a href="pastor.php">Our Pastor</a></li>
