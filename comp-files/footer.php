cb7068a621fd02541212b995ba4fa993b5bd08b4
tjohn252 <tjohn252@uncc.edu>
2020-12-31 21:53:12 -0500

    adjusted footer layout and got rid of line

--- comp-files/footer.php
+++ comp-files/footer.php
@@ -1,10 +1,6 @@
 <footer>
 
-<div class="legal-stuff">
-  <a onclick="window.open('privacy.php', 'name','width=600,height=400')">Privacy</a>
-  <a onclick="window.open('terms.php', 'name','width=600,height=400')">Terms</a>
-  <a href="https://www.cogic.org/" target="_blank">COGIC Website</a>
-</div>
+
 
 <div class="social-media">
   <a href="https://facebook.com/sycamoretemple">
@@ -22,8 +18,14 @@
 
 <div class="info">
   11 N Ann Street, Asheville, NC 28803 <br>
-  123-456-7890 <br><br>
+  (828)-253-2968 <br>
   © 2020 Sycamore Temple Church of God in Christ, Inc. - All Rights Reserved.
+</div>
+
+<div class="legal-stuff">
+  <a onclick="window.open('privacy.php', 'name','width=600,height=400')">Privacy</a>
+  <a onclick="window.open('terms.php', 'name','width=600,height=400')">Terms</a>
+  <a href="https://www.cogic.org/" target="_blank">COGIC Website</a>
 </div>
 
 </footer>
