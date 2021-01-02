33d863e7ef4f1817a28fee7893ac29160aecda66
tjohn252 <tjohn252@uncc.edu>
2021-01-01 20:35:12 -0500

    added giving and ministry pages

---
+++ giving.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+  <!-- Header info from header.php -->
+  <head>
+    <title>Sycamore Temple COGIC | Home</title>
+    <?php include "comp-files/header.php"; ?>
+    <style>
+      #nav-[] { background-color: var(--green);}
+      #nav-[] a { color: #fff;}
+    </style>
+
+  </head>
+
+  <body>
+    <div class="page-container">
+      <!-- navigation from menu.php -->
+      <?php include "comp-files/menu.php"; ?>
+
+      <div class="content">
+
+        <!-- Content Begins Here -->
+
+
+
+
+        <!-- Content Ends Here -->
+
+      </div>
+      <!-- Footer from footer.php -->
+      <?php include "comp-files/footer.php"; ?>
+    </div>
+
+
+  </body>
+  <?php include "comp-files/scripts.php"; ?>
+</html>
