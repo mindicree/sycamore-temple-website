124899619791da4fdf93ce5e720a6faca6c72e8f
tjohn252 <tjohn252@uncc.edu>
2021-01-01 19:05:25 -0500

    replaced seal with HQ version

--- index.php
+++ index.php
@@ -67,7 +67,7 @@
           <!--Quick Link 1 -->
           <a href="#" style="text-decoration: none;"> <div class="quick-link" id="quick-link-3">
             <span class="quick-link-text"> Ministries </span>
-            <img src="img\assets\seal.png" alt="" style="width: 120px; height: 120px; margin: auto" id="seal">
+            <img src="img\assets\seal_2.png" alt="" style="width: 128px; height: 128px; margin: auto" id="seal">
           </div> </a>
 
           <!--Quick Link 1 -->
