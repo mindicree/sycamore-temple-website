473ccd44218189906db3bbdffa5b533872d9c55e
tjohn252 <tjohn252@uncc.edu>
2021-01-02 20:44:29 -0500

    removed present and future in about page temporarily

--- about.php
+++ about.php
@@ -23,12 +23,12 @@
           <h1>About Us</h1>
           <div class="about-text-wrapper">
 
-
+            <!--
             <h2 class="about-title">Sycamore Temple Today</h2>
-            <p>&emsp; &emsp; Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vel consectetur dolor. Fusce tempus, quam sit amet rutrum venenatis, ex nibh auctor est, in hendrerit metus lectus at arcu. Suspendisse sagittis nisi in ex consequat lacinia. Duis malesuada velit ut est suscipit fermentum. Donec odio lorem, mattis sed eros quis, volutpat condimentum erat. Mauris et ullamcorper justo. Aliquam vel cursus purus, a congue elit. Nunc dignissim, mauris in tempus faucibus, dui risus sodales ligula, sit amet feugiat tellus ipsum non nunc. Nullam eu eleifend lorem. Nunc pellentesque sodales ipsum, nec hendrerit velit condimentum non. Mauris sagittis turpis libero, a finibus mi lobortis id. Aenean tincidunt massa a eleifend feugiat. Donec condimentum dolor vel eros bibendum aliquam. Ut consectetur, mi laoreet consequat iaculis, tellus dui sodales magna, sit amet egestas nunc lorem ac tortor. Fusce libero odio, dictum nec neque quis, aliquam porta felis. Curabitur in vulputate metus. <br>&emsp; &emsp;
+            -->
+            <p>
 
-  Suspendisse ante dolor, volutpat et enim lacinia, mattis eleifend orci. Pellentesque luctus fringilla rhoncus. Vivamus eget arcu id risus ullamcorper interdum a sit amet massa. Etiam laoreet ut lectus ut tristique. Maecenas nec fermentum tellus. Ut lorem orci, interdum sed justo quis, suscipit convallis tellus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aliquam lorem sapien, scelerisque ac turpis pellentesque, condimentum commodo leo.</p>
-
+            </p>
 
             <h2>Our History</h2>
 
@@ -42,9 +42,12 @@
 
             </p>
 
+            <!--
             <h2>Our Future</h2>
+            -->
+            <p>
 
-            <p>&emsp; &emsp; Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque lacinia turpis ut arcu fermentum, eget lacinia ligula finibus. Fusce posuere est sodales erat sagittis, sit amet posuere quam hendrerit. Cras convallis ex quis neque dictum volutpat in ut libero. In rhoncus lectus sed velit sagittis, id tristique neque ultricies. Nunc non nunc in lectus laoreet lobortis. Nullam egestas velit cursus, euismod massa quis, suscipit nisi. Proin ultrices bibendum neque, non auctor odio consequat eget. Phasellus consectetur nunc sit amet sapien lobortis aliquet. Curabitur vel tellus id mi dictum volutpat at eu nunc. Vivamus dui odio, pellentesque et justo sed, semper dapibus felis. Sed cursus erat nec nulla commodo faucibus. Ut in elit non arcu sodales maximus sit amet non leo. Proin ac interdum sem.</p>
+            </p>
           </div>
 
 
